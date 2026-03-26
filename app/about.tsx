72cb24da31ba85f65aa161161f8de1408c700b8d
rksdeveloper <raj7234271@gmail.com>
2026-03-27 00:49:24 +0530

    updated file

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const about = () => {
+  return (
+    <View>
+        <Text>This is About Screen</Text>
+    </View>
+  )
+}
+
+export default about
---
+++ app/index.tsx
@@ -0,0 +1,40 @@
+
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container} 
+    >
+      <Text style={textStyles.text1}>Welcome to Trackza</Text>
+      <Text style={textStyles1.text2}>Kharcha Likho Baki Trackza Smjhega</Text>
+      <Link>This is About Screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'#fff',
+   gap:30
+    
+  }
+})
+const textStyles = StyleSheet.create({
+  text1:{
+    fontSize:30,
+    color:'black',
+    // backgroundColor:'#175b70'
+  }
+})
+const textStyles1 = StyleSheet.create({
+  text2:{
+    fontSize: 16,
+    color: 'grey',
+    // backgroundColor:'#FFF'
+  }
+})
